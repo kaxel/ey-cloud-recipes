5533978f58009d93b6d4cac9fd646df0bd38e6c5
Krister Axel <krister.axel@gmail.com>
2015-10-26 09:55:02 -0700

    add ban

--- cookbooks/ban/recipes/default.rb
+++ cookbooks/ban/recipes/default.rb
@@ -3,8 +3,8 @@
 # Recipe:: default
 #
 
-# ban('ban name') do
-#   ip "128.23.83.192"
-#   ip "243.123.123.123", :ports => [22, 80, 443]
-#   ip "222.0.0.0/8"
-# end
+ban('blacklist') do
+  ip "54.76.18.23"
+  ip "36.22.100.135"
+  ip "178.220.94.5"
+end
